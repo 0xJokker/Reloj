b023148d1e4b54893581ac85712274863e3ef1fd
ESepulvedaV <129913584+JokerC0@users.noreply.github.com>
2023-05-18 21:17:43 -0500

    upload_reloj

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+function reloj() {
+	time = new Date();
+	horas = time.getHours();
+	minutos = time.getMinutes();
+	segundos = time.getSeconds();
+    
+    if (horas >= 12) {
+     	porcentajeHoras = horas / 12 * 360;
+    }  else {
+    	porcentajeHoras = horas / 24 * 360;
+    }
+
+    // Aumentamos cada aguja y el el resultado de cada hora, minuto, segundo
+    porcentajeHoras += minutos / 60 * 30;
+    porcentajeMinutos = minutos / 60 * 360;
+    porcentajeSegundos = segundos / 60 * 360;
+
+    document.getElementById("horas").style.transform = "rotate("+ porcentajeHoras +"deg)";
+    document.getElementById("minutos").style.transform = "rotate("+ porcentajeMinutos +"deg)";
+    document.getElementById("segundos").style.transform = "rotate("+ porcentajeSegundos +"deg)";
+    document.getElementById("p-content").innerHTML = horas + ":" + minutos + ":" + segundos; 
+}
+
+
+setInterval(reloj, 1000);
